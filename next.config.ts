3c47a6ca9518f4a18374a1720699d1628dea278e
Josephus Sarsonas <sarsonasjosephuskim@gmail.com>
2025-07-29 13:07:59 +0000

    make sure to accept any domain for now

--- next.config.ts
+++ next.config.ts
@@ -13,16 +13,8 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
+        hostname: '**',
       },
-      {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
-        port: '',
-        pathname: '/**',
-      }
     ],
   },
 };
