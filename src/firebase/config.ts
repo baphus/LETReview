98a74a6d2e9c77f9644ac49bf33e4aaa87969b6c
Josephus Sarsonas <sarsonasjosephuskim@gmail.com>
2026-01-19 17:57:24 +0000

    Access blocked: This app’s request is invalid sarsonasjosephuskim@gmail.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "projectId": "letreview",
   "appId": "1:719794505288:web:8f4da0edb8335483b27a7d",
   "apiKey": "AIzaSyDCLLr_g6MBdQbJvQ-puM8bXbI0L4icF3Q",
-  "authDomain": "studio--letreview.us-central1.hosted.app",
+  "authDomain": "letreview.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "719794505288"
 };
