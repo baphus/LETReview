32c9c663e6562cccec65bf48b3aeb4c1137ea99f
Josephus Sarsonas <sarsonasjosephuskim@gmail.com>
2026-01-19 18:39:31 +0000

    ------------------------------------------------------------------------

---
+++ next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/__/:path*",
+        destination: "https://letreview.firebaseapp.com/__/:path*",
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
