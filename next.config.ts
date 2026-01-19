36f5eafbee28d256fa5f598e1568d40ac6fc1a57
Josephus Sarsonas <sarsonasjosephuskim@gmail.com>
2026-01-19 20:37:46 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-
 // This file is intentionally left blank. 
 // The Next.js configuration has been moved to next.config.js to resolve a build error.
 // The build environment does not support TypeScript for Next.js configuration.
