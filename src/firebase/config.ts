d08e14500b6570a645cb8b5c5ee88dd110b0fddd
Josephus Sarsonas <sarsonasjosephuskim@gmail.com>
2026-01-20 16:17:45 +0000

    Set up a Firebase project

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,15 @@
 export const firebaseConfig = {
-  "apiKey": "AIzaSyDCLLr_g6MBdQbJvQ-puM8bXbI0L4icF3Q",
-  "authDomain": "letreview.firebaseapp.com",
-  "projectId": "letreview",
-  "appId": "1:719794505288:web:8f4da0edb8335483b27a7d",
-  "messagingSenderId": "719794505288",
-  "measurementId": "G-9X6253W4S6"
+
+  apiKey: "AIzaSyDCLLr_g6MBdQbJvQ-puM8bXbI0L4icF3Q",
+
+  authDomain: "letreview.firebaseapp.com",
+
+  projectId: "letreview",
+
+  storageBucket: "letreview.firebasestorage.app",
+
+  messagingSenderId: "719794505288",
+
+  appId: "1:719794505288:web:70fd1f9a0d58d1aeb27a7d"
+
 };
