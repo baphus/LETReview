ba0caeeaf1d0bd8e41bd0304e0e47545622ab786
Josephus Sarsonas <sarsonasjosephuskim@gmail.com>
2026-01-22 14:37:17 +0000

    try including this one
    <link
          rel="stylesheet"
          href="https:/

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -137,8 +137,8 @@ export default function RootLayout({
         />
         <link
           rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/katex@0.16.10/dist/katex.min.css"
-          integrity="sha384-hIoDRK3BOKdib2SgYJymby3d+gDRRQWPzEdi1o4pG4_zL4p3S/tYCEFj+3qG4QCE"
+          href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css"
+          integrity="sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc"
           crossOrigin="anonymous"
         />
       </head>
